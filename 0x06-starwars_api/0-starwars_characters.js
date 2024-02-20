7ba2ec779b48c7ee19f95897a939a8bf158d9740
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-02-20 02:20:58 +0200

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url, (err, res, body) => {
+request(url, async (err, res, body) => {
   if (err) {
     console.error(err);
     return;
@@ -11,27 +11,28 @@ request(url, (err, res, body) => {
   if (res.statusCode === 200) {
     const data = JSON.parse(body);
     const characters = data.characters;
-    characters.forEach(async (characterUrl) => {
+    for (const characterUrl of characters) {
       const name = await fetchName(characterUrl);
-    });
+      console.log(name);
+    }
   } else {
     console.error(res.statusCode);
   }
 });
 
-async function fetchName(url) {
-  return new Promise((res, rej) => {
+async function fetchName (url) {
+  return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (error) {
         reject(error);
         return;
       }
       if (response.statusCode !== 200) {
-        reject(`${response.statusCode}`);
+        reject(response.statusCode);
         return;
       }
       const characterData = JSON.parse(body);
-      console.log(characterData.name);
+      resolve(characterData.name);
     });
   });
 }
